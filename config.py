243fe11162b0746bcc0a22f4f72cb67bdca6a0ea
Owl <scalearray@tutanota.com>
2026-02-28 08:51:10 +0100

    Update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 DEVELOPER_MODE = True
-APP_VERSION = '0.4.5'
+APP_VERSION = '0.4.4'
 
 # --- Load .env file ---
 _env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.env')
